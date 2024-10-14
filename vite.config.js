8bbdebbfe656c654d0b01233461271dcc7b42dce
Dmytro Bulakhov <bulakhovda@gmail.com>
2024-10-14 17:52:04 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,56 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
 
-  server: {
-    open: true,
-  },
+    root: 'src',
+
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+
+          assetFileNames: assetInfo => {
+            if (assetInfo.names && assetInfo.names.endsWith('.html')) {
+              return '[names].[ext]';
+            }
+            return 'assets/[names]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+
+      emptyOutDir: true,
+    },
+
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
 });
